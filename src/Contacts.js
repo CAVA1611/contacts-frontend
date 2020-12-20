c7d7434407689f869728f6cc4f799bc5b74e7f9d
Cesar Velasco Arias <cesar091611pm@gmail.com>
2020-12-20 16:39:03 -0500

    Edit and delete contact

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,32 +1,97 @@
 import React from 'react';
 import Contact from './Contact.js';
 import Alert from './Alert.js';
+import NewContact from './NewContact.js';
+import EditContact from './EditContact.js';
 
 class Contacts extends React.Component {
     constructor(props)  {
         super(props);
         this.state = {
-            selectedContact: null
+            errorInfo: null,
+            contacts: this.props.contacts,
+            isEditing: {}
         }
         this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
         this.handleCloseError = this.handleCloseError.bind(this);
+        this.addContact = this.addContact.bind(this);
     }
 
     handleEdit(contact) {
-        this.setState({
-            selectedContact: contact.name
-        });
+        this.setState(prevState => ({
+            isEditing: {...prevState.isEditing, [contact.name]: contact}
+            //errorInfo: contact.name
+        }));
 
     }
+
+
+    handleDelete(contact) {
+        this.setState(prevState => ({
+            contacts: prevState.contacts.filter((c) => c.name !== contact.name)
+        }))
+    }
+
+    handleCancel(name, contact) {
+        this.setState(prevState => {
+            const isEditing = Object.assign({}, prevState.isEditing);
+            delete isEditing[name];
+            return {
+                isEditing: isEditing
+            }
+        })
+    }
+
+    handleChange(name, contact) {
+        this.setState(prevState => ({
+            isEditing: {...prevState.isEditing, [name]: contact}
+        }))
+    }
+
+    handleSave(name, contact) {
+        this.setState(prevState =>{
+            const isEditing = Object.assign({}, prevState.isEditing);
+            delete isEditing[name];
+            if(name === contact.name) {
+                const contacts = prevState.contacts;
+                const pos = contacts.findIndex(c => c.name === contact.name);
+                return {
+                    contacts: [...contacts.slice(0,pos), Object.assign({}, contact), ...contacts.slice(pos+1)],
+                    isEditing: isEditing
+                }
+            }
+            return {
+                errorInfo: "Cannot edit Name"
+            }
+
+        });
+    }
+
+
     handleCloseError () {
         this.setState({
-            selectedContact: null
+            errorInfo: null
+        });
+    }
+
+    addContact(contact){
+        this.setState(prevState => {
+            const contacts = prevState.contacts;
+            if (! contacts.find(c => c.name === contact.name)) {
+                return ({
+                    contacts: [...prevState.contacts,contact]
+                });
+            }
+            return({
+                errorInfo: 'Contact already exists'
+            });
         });
     }
     render() {
         return(
             <div>
-                    <Alert message={this.state.selectedContact} onClose={this.handleCloseError}/>
+                    <Alert message={this.state.errorInfo} onClose={this.handleCloseError}/>
                 <table class="table">
                     <thead>
                         <tr>
@@ -35,8 +100,17 @@ class Contacts extends React.Component {
                             <th>&nbsp;</th>
                         </tr>
                     </thead>
-                    {this.props.contacts.map((contact) =>
-                        <Contact contact = {contact} onEdit={this.handleEdit}/>
+                    <NewContact onAddContact={this.addContact}/>
+                    {this.state.contacts.map((contact) =>
+                        ! this.state.isEditing[contact.name] ?
+                        <Contact key={contact.name} contact = {contact} 
+                            onEdit={this.handleEdit}
+                            onDelete={this.handleDelete}/>
+                        :
+                        <EditContact key={contact.name} contact = {this.state.isEditing[contact.name]}
+                            onCancel={this.handleCancel.bind(this, contact.name)}
+                            onChange={this.handleChange.bind(this, contact.name)}
+                            onSave={this.handleSave.bind(this, contact.name)}/>
                     )}
                 </table>
             </div>
