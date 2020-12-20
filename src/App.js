ca2242991e5f7908844c34fd30a5861370be59cf
Cesar Velasco Arias <cesar091611pm@gmail.com>
2020-12-20 09:26:11 -0500

    Add Contac.js file and edit App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Contact from './Contact.js';
 
 function App() {
+  const contact = {
+    name: "Andres",
+    phone: "0987534265"
+  } ;
+
   return (
+  <div>
     <h1>Hello Word ..!!</h1>
+    <Contact contact={contact}/>
+  </div>
   );
 }
 
